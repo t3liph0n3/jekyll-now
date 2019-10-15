efee215adf62399ae8866b267920ee0cdf7eb74e
t3liph0n3 <hyperstn@outlook.jp>
2019-10-15 16:21:54 +0800

    100 days of code start

--- _r2-log/today.sh
+++ _r2-log/today.sh
@@ -7,7 +7,7 @@ c=$((($(date +%s)-$(date +%s --date "2019-10-15"))/(3600*24)))
 echo what is the title of this post?
 read NAME
 if [ -n "$NAME" ] && [ "$NAME" = "${NAME//[ ]}" ]; then
-    header=$'---\nlayout: post\ntitle: day-'$c$' '$NAME$'\n---\n\n'
+    header=$'---\nlayout: post\ntitle: d-'$c$'r2-'$NAME$'\n---\n\n'
     echo "$header" >  $d-d$c-$NAME.md
     nano $d-d$c-$NAME.md
 else
